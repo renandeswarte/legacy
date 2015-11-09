3e27860e420263f20d4f38d2b81353720d411109
Ryan James <ryanwilsonjames@gmail.com>
2015-11-09 11:02:54 -0800

    feat: checkout toggle and submits work, modal on completion works

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -5,66 +5,89 @@ angular.module('instacutz.auth', [])
   '$window',
   '$location',
   'Auth',
-  function($scope, $window, $location, Auth) {
+  'Order',
+  function($scope, $window, $location, Auth, Order) {
 
-  $scope.user = {}; //this is attached to ng-model in the view
-  $scope.failedAttempt = false;
-  $scope.failedLogin = false;
-  $scope.signedIn = false;
-  $scope.Loginorout = Auth;
+    $scope.user = {}; //this is attached to ng-model in the view
+    $scope.failedAttempt = false;
+    $scope.failedLogin = false;
+    $scope.signedIn = false;
+    $scope.Loginorout = Auth;
 
-  $scope.consoleTheUser = function() {
-    console.log('this is the current user: ', $scope.user);
-  };
+    // $scope.stripeCallback = function(code, result) {
+    //   if (result.error) {
+    //     window.alert('Payment failed. Error: ' + result.error.message);
+    //   } else {
+    //     console.log('Token success! Token: ' + result.id);
+    //     Order.stripeTokenSubmit(result.id)
+    //       .then(function(response) {
+    //         console.log('Payment completed successfully.', response);
+    //         $location.path('/'); // TODO: redirect to payment success page/modal
+    //       }, function(error) {
+    //         console.log('Failed, error: ', error);
+    //       });
+    //   }
+    // };
 
-  $scope.signup = function() {
-    console.log('currently signing up: ', $scope.user);
-    Auth.signup($scope.user)
-      .then(function(token) {
-        $window.localStorage.setItem('com.semicolon', token);
-        $window.localStorage.setItem('com.semicolon.name', $scope.user.username);
-        $window.localStorage.setItem('com.semicolon.date', new Date());
-        $location.path('/order');
-      })
-      .catch(function(err) {
-        $scope.failedAttempt = true;
-        console.log(err);
-      });
-  };
+    $scope.consoleTheUser = function() {
+      console.log('this is the current user: ', $scope.user);
+    };
 
-  $scope.signin = function() {
-    console.log('signin is called');
-    Auth.signin($scope.user)
-      .then(function(respData) {
-        $scope.user = respData.user;
-        $scope.signedIn = true;
-        console.log('user: ', $scope.user);
-        $window.localStorage.setItem('com.semicolon', respData.token);
-        $window.localStorage.setItem('com.semicolon.name', $scope.user.username);
-        $window.localStorage.setItem('com.semicolon.date', new Date());
-        Auth.loginorout = "Logout"
-        // $location.path('/order');
-      })
-      .catch(function(err) {
-        $scope.failedLogin = true;
-        console.log(err);
+    $scope.returnSignin = function() {
+      $scope.$apply();
+      return $scope.signedIn;
+    };
 
-      });
-  };
+    $scope.signup = function() {
+      console.log('currently signing up: ', $scope.user);
+      Auth.signup($scope.user)
+        .then(function(token) {
+          $window.localStorage.setItem('com.semicolon', token);
+          $window.localStorage.setItem('com.semicolon.name', $scope.user.username);
+          $window.localStorage.setItem('com.semicolon.date', new Date());
+          $location.path('/order');
+        })
+        .catch(function(err) {
+          $scope.failedAttempt = true;
+          console.log(err);
+        });
+    };
 
-  $scope.signout = function() {
-    if (Auth.loginorout === "Logout") {
-      Auth.signout();
-    } else {
-      $location.path("/signin");
-    }
-  };
+    $scope.signin = function() {
+      console.log('signin is called');
+      Auth.signin($scope.user)
+        .then(function(respData) {
+          $scope.user = respData.user;
+          $scope.signedIn = true;
+          console.log('user: ', $scope.user);
+          console.log('signedIn: ', $scope.signedIn);
+          $window.localStorage.setItem('com.semicolon', respData.token);
+          $window.localStorage.setItem('com.semicolon.name', $scope.user.username);
+          $window.localStorage.setItem('com.semicolon.date', new Date());
+          Auth.loginorout = "Logout";
+            // $location.path('/order');
+        })
+        .catch(function(err) {
+          $scope.failedLogin = true;
+          console.log(err);
 
-  $scope.getUsername = function() {
-    return Auth.getUsername();
-  };
+        });
+    };
 
-}])
+    $scope.signout = function() {
+      if (Auth.loginorout === "Logout") {
+        Auth.signout();
+      } else {
+        $location.path("/signin");
+      }
+    };
+
+    $scope.getUsername = function() {
+      return Auth.getUsername();
+    };
+
+  }
+])
 
 // get meals db.users.find()
 // users.insert({username:'bob',password:'hashword',salt:'NaCl',meals:[{title:'phad thai',price:'12'},{title:'chicken rice',price:'10’},{title:'chicken rice',price:'10’},{title:'chicken rice',price:'10'}],orders:{}})
--- client/app/order/order.js
+++ client/app/order/order.js
@@ -1,7 +1,7 @@
 angular.module('instacutz.order', [
   'angularPayments',
   'instacutz.auth'
-  ])
+])
 
 // .config(['$window', function($window) {
 //   $window.Stripe.setPublishableKey('pk_test_kL2LKwSIme8Q6KzTiLQ2ZPHz');
@@ -15,27 +15,28 @@ angular.module('instacutz.order', [
   'Counter',
   'Auth',
   function($scope, $window, $location, Order, Counter, Auth) {
-      //Parse order inside local storage first
-      $scope.orderRequest = JSON.parse($window.localStorage.getItem("order"));
+    //Parse order inside local storage first
+    $scope.orderRequest = JSON.parse($window.localStorage.getItem("order"));
 
-      $scope.requestStyle = $scope.orderRequest.orders[0].styleName;
-      $scope.requestStylePic = $scope.orderRequest.orders[0].stylePicture
-      $scope.requestPrice = $scope.orderRequest.orders[0].stylePrice;
-      $scope.requestBarber = $scope.orderRequest.orders[0].barberName;
+    $scope.requestStyle = $scope.orderRequest.orders[0].styleName;
+    $scope.requestStylePic = $scope.orderRequest.orders[0].stylePicture
+    $scope.requestPrice = $scope.orderRequest.orders[0].stylePrice;
+    $scope.requestBarber = $scope.orderRequest.orders[0].barberName;
 
     $scope.stripeCallback = function(code, result) {
-        if (result.error) {
-            window.alert('Payment failed. Error: ' + result.error.message);
-        } else {
-            console.log('Token success! Token: ' + result.id);
-            Order.stripeTokenSubmit(result.id)
-            .then(function(response) {
-              console.log('Payment completed successfully.', response);
-              $location.path('/');  // TODO: redirect to payment success page/modal
-            }, function(error) {
-              console.log('Failed, error: ', error);
-            });
-        }
+      if (result.error) {
+        window.alert('Payment failed. Error: ' + result.error.message);
+      } else {
+        console.log('Token success! Token: ' + result.id);
+        Order.stripeTokenSubmit(result.id)
+          .then(function(response) {
+            console.log('Payment completed successfully. (in order.js)', response);
+            $('#myModal').modal('toggle');
+            $location.path('/'); // TODO: redirect to payment success page/modal
+          }, function(error) {
+            console.log('Failed, error: ', error);
+          });
+      }
     };
 
     $scope.captureSignUpDetails = function() {
@@ -64,7 +65,7 @@ angular.module('instacutz.order', [
 
     $scope.orders = JSON.parse($window.localStorage.getItem('order'));
 
-    $scope.checkOrder = function() {  // redirect if not logged in
+    $scope.checkOrder = function() { // redirect if not logged in
       if ($scope.orders.orders.length === 0) {
         $location.path("/");
       }
@@ -92,16 +93,16 @@ angular.module('instacutz.order', [
     }
 
     $scope.RemoveItem = function(array, index) {
-      var order = JSON.parse($window.localStorage.getItem("order"));
-      console.log(order);
-      array.splice(index, 1);
-      order.orders.splice(index, 1);
-      $window.localStorage.setItem("order", JSON.stringify(order));
-      if (order.orders.length === 0) {
-        $location.path('/barbers')
+        var order = JSON.parse($window.localStorage.getItem("order"));
+        console.log(order);
+        array.splice(index, 1);
+        order.orders.splice(index, 1);
+        $window.localStorage.setItem("order", JSON.stringify(order));
+        if (order.orders.length === 0) {
+          $location.path('/barbers')
+        }
+        Counter.number--
       }
-      Counter.number--
-    }
-    // $scope.checkOrder(); // redirect if not logged in
+      // $scope.checkOrder(); // redirect if not logged in
   }
 ]);
